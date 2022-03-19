1cb2e4a646ecfc88ebda38d58e0ae6e0e935fbe8
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-19 12:13:59 +0800

    feat:实现封装自定义对话框

---
+++ frontend/wbapp/src/components/common/DialogueBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bg" v-if="dialogShow">
+    <div class="box">
+      <div class="title">
+        <h2><slot name="title">确认删除么？</slot></h2>
+      </div>
+      <div class="content"><slot name="content">内容</slot></div>
+      <div class="btn">
+        <button class="cancel" @click="cancel">取消</button>
+        <button class="yes" @click="toDel">确认</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogShow: true,
+    };
+  },
+  emits: ["hideDialog", "yes"],
+  methods: {
+    cancel() {
+      this.$emit("hideDialog");
+    },
+    toDel() {
+      this.$emit("yes");
+      this.$emit("hideDialog");
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.bg {
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+  height: 100%;
+  width: 100%;
+  color: cornsilk;
+  background: rgba(0, 0, 0, 0.4);
+}
+.box {
+  width: 300px;
+  height: 200px;
+  z-index: 200;
+  border-radius: 5px;
+  color: black;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  background: whitesmoke;
+  padding: 30px 0;
+}
+.box .btn {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.box .btn button {
+  width: 90px;
+  height: 40px;
+  font-size: 20px;
+  outline: none;
+  border: 0;
+  border-radius: 20px;
+  transition: all ease 0.3s;
+}
+.box .btn button:hover {
+  border: 2px solid black;
+}
+.box .btn button:active {
+  transform: translateY(3px);
+}
+.box .btn button:nth-child(2) {
+  background: royalblue;
+}
+</style>
