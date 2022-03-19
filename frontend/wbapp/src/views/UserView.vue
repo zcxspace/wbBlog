30cd575f578f1ccdf260b043c4a1c2d9f2faa668
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-19 19:52:27 +0800

    feat: 实现搭建登录用户页

---
+++ frontend/wbapp/src/views/UserView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="User">
+    <top-nav-bar>
+      <template #center1>中1</template>
+
+      <template #center2></template>
+
+      <template #center3></template>
+
+      <template #center4></template>
+
+      <template #center5> </template>
+
+      <template #right1></template>
+      <template #right2> </template>
+      <template #right3> </template>
+    </top-nav-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TopNavBar from "../components/common/TopNavBar.vue";
+
+export default {
+  components: { TopNavBar },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
