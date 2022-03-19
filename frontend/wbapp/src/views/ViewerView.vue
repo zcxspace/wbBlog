485d4948b1a4f788749181bc8020122a7e34b6f9
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-19 11:35:06 +0800

    feat:实现自定义元素区

---
+++ frontend/wbapp/src/views/ViewerView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Viewer">
+    <main-lay-out :isShowTop="isShowTop"> </main-lay-out>
+  </div>
+</template>
+
+<script>
+import MainLayouts from "../layouts/MainLayout.vue";
+export default {
+  data() {
+    return {
+      isShowTop: true, //显示头部背景区
+    };
+  },
+  components: {
+    MainLayouts,
+    MainLayOut,
+  },
+};
+</script>
+
+<style scoped>
+</style>
