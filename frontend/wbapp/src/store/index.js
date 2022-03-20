5da0e7c313be0533744df99fa4a8a292a3d3e42c
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-20 12:18:18 +0800

    feat:实现登录用户信息的储存

--- frontend/wbapp/src/store/index.js
+++ frontend/wbapp/src/store/index.js
@@ -2,10 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userInfo: null,
+
   },
   getters: {
   },
   mutations: {
+    //更新当前登录用户的数据
+    updateUserInfo(state, info) {
+      state.userInfo = info
+    },
+    //退出登录时清空所有数据
+    resetInfo(state) {
+      state.userInfo = null;
+    }
   },
   actions: {
   },
