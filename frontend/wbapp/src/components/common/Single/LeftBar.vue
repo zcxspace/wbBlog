d565262ee2afc82a053f5151ceedc7fac64a7ab1
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-21 23:14:01 +0800

    feat: 实现封装侧边栏组件

---
+++ frontend/wbapp/src/components/common/Single/LeftBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="leftBar">
+    <div class="title">
+      <h2><slot name="title">侧边栏标题</slot></h2>
+    </div>
+    <div class="tabs">
+      <ul class="tabBox">
+        <!-- <li v-for="tab of tabsArr" :key="tab.com"> -->
+        <!-- <div class="icon"><i class="inconfont" :class="tab.icon"></i>字体图标</div> -->
+        <li
+          v-for="tab of tabsArr"
+          :key="tab.comName"
+          @click="nowTabName = tab.comName"
+          :class="[nowTabName == tab.comName ? 'active' : '']"
+        >
+          <div class="icon">字体</div>
+          {{ tab.comName }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabsArr: Array,
+  },
+  data() {
+    return {
+      nowTabName: this.tabsArr[0].comName,
+    };
+  },
+  methods: {
+    sentComName() {
+      return this.nowTabName;
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+li {
+  list-style: none;
+}
+.leftBar {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  width: 100%;
+  height: 50px;
+  padding: 10px 16px;
+  display: flex;
+  justify-content: flex-start;
+}
+.tabs {
+  width: 100%;
+}
+.tabBox {
+  flex: 1;
+  width: 100%;
+  height: 100px;
+}
+.tabBox li {
+  width: 100%;
+  height: 45px;
+  font-size: 20px;
+  font-weight: 800;
+  display: flex;
+  border-radius: 5px;
+  align-items: center;
+  transition: all ease 0.3s;
+}
+.active {
+  color: rgb(199, 132, 9);
+}
+.tabBox li:hover {
+  background: rgb(228, 226, 226);
+}
+.icon {
+  margin: 0 15px;
+}
+</style>
