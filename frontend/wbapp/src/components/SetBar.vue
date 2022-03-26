bdc6a5a9f43be6821064c134bf74ba9958af604d
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-26 11:34:06 +0800

    fix:修复无图片动态无法转发问题

--- frontend/wbapp/src/components/SetBar.vue
+++ frontend/wbapp/src/components/SetBar.vue
@@ -1,10 +1,12 @@
 <template>
   <div :class="[showBack ? 'back' : '']" v-if="isShow">
+    <!-- 如果不显示黑色背景则为静态发布块 宽度100% -->
     <div class="setBar" v-if="true" :class="[showBack ? 'shotBarWidth' : '']">
+      <!-- 对话框 -->
       <dialogue-box @yes="deleteAll" @hideDialog="confirm" v-show="dialogShow">
         <template #content>编辑内容将全部消失</template>
       </dialogue-box>
-
+      <!-- 发布块标题 -->
       <div class="top" v-if="showTop">
         <div><slot name="title">快捷发布</slot></div>
         <div>
@@ -13,22 +15,22 @@
           </button>
         </div>
       </div>
-
+      <!-- 输入文字区 -->
       <div class="text">
         <el-input
-          class="e-input"
           v-model="textarea"
-          :autosize="{ minRows: 4, maxRows: 7 }"
+          :autosize="{ minRows: this.textareaHeight, maxRows: 7 }"
           type="textarea"
           resize="none"
-          placeholder="有什么新鲜事想分享给大家？ "
+          :placeholder="placeholder"
         />
       </div>
-      <slot :textarea="textarea"></slot>
+      <!-- <slot :textarea="textarea"></slot> -->
 
-      <div class="img" v-if="urls.length != 0">
+      <div class="img" v-if="displayUrls.length != 0">
         <ul>
-          <template v-for="(url, index) of urls" :key="index">
+          <!-- 图片展示区 -->
+          <template v-for="(url, index) of displayUrls" :key="index">
             <li class="imgBox">
               <img :src="url" alt="自定义" />
               <button @click="delImg(index)">
@@ -36,7 +38,7 @@
               </button>
             </li>
           </template>
-          <li class="addBox" v-if="urls.length < 3">
+          <li class="addBox" v-if="displayUrls.length < 3">
             <i class="confont">字体</i>
             <input type="file" @change="getUrl" />
           </li>
@@ -44,13 +46,16 @@
       </div>
 
       <div class="bottom">
-        <div class="addBtn" v-if="urls.length < 3">
-          <i class="iconfont">add</i> <input type="file" @change="getUrl" />
+        <div class="addBtn" v-if="displayUrls.length < 3">
+          <i class="iconfont">add</i>
+          <input type="file" @change="getUrl" multiple="multiple" />
         </div>
         <div v-else>最多添加九张图片哦～</div>
 
         <div>
-          <button class="postBtn" @click="postNew">发送</button>
+          <button class="postBtn" @click="postNew">
+            <slot name="btnName">发送</slot>
+          </button>
         </div>
       </div>
     </div>
@@ -59,18 +64,24 @@
 
 <script>
 import DialogueBox from "../components/common/DialogueBar.vue";
-import { SentBlog } from "../assets/request/index.js";
+import { SentBlog, getUserInfo, GetPublic } from "../assets/request/index.js";
 import { mapMutations } from "vuex";
 export default {
   props: {
+    useTo: String,
     showTop: Boolean,
     showBack: Boolean,
     hideAfterSent: Boolean,
     editInfo: Object,
+    textareaHeight: Number,
+    placeholder: String,
+    forwardId: Number,
+    editId: Number,
   },
   data() {
     return {
-      urls: [],
+      displayUrls: [],
+      postUrls: [],
       url: "",
       textarea: "",
       isShow: true,
@@ -84,26 +95,31 @@ export default {
     getUrl(e) {
       console.log("我执行了");
       let file = e.target.files[0];
+      this.postUrls.push(file);
       console.log(file);
       let that = this;
+      //获取图片进行回显
       let reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = function () {
-        console.log(that.urls.length);
-        that.urls.push(this.result);
+        console.log(that.displayUrls.length);
+        that.displayUrls.push(this.result);
+        console.log(that.displayUrls);
+        console.log(that.postUrls);
       };
-      console.log(this.urls);
+      console.log(this.displayUrls);
+      console.log(this.postUrls);
     },
 
     //删除已经添加的图片
     delImg(index) {
-      this.urls.splice(index, 1);
-      console.log(this.urls.length);
+      this.displayUrls.splice(index, 1);
+      console.log(this.displayUrls.length);
     },
     // 有数据后 删除提示
     confirm() {
       //若有数据则提示 否则直接删除
-      if (this.urls.length == 0 && this.textarea == "") {
+      if (this.displayUrls.length == 0 && this.textarea == "") {
         this.$emit("SetBar");
         console.log("隐藏");
       } else {
@@ -113,7 +129,7 @@ export default {
 
     // 清除发布框数据
     deleteAll() {
-      this.urls = [];
+      this.displayUrls = [];
       this.textarea = "";
       this.dialogShow = !this.dialogShow;
     },
@@ -124,15 +140,45 @@ export default {
     //   console.log("已经发送了" + this.inputName);
     // },
 
-    ...mapMutations(["addData"]),
+    ...mapMutations(["addData", "updateUserInfo", "updateHomePageDynamic"]),
     //测试发布模块
     async postNew() {
       if (this.textarea) {
+        let form = new FormData();
+        for (let url of Object.values(this.postUrls)) {
+          form.append("file", url);
+        }
+
+        console.log(form.getAll("file"));
         let userId = this.$store.state.userInfo.id;
-        console.log(userId);
-        await SentBlog(this.textarea, userId);
+
+        form.append("text", this.textarea);
+        form.append("visible", 0);
+        form.append("userId", userId);
+        //发布功能
+        if (this.useTo == "publish") {
+          form.append("forwardDynamicId", 0);
+          await SentBlog(form);
+          //发送动态后获取用户信息
+          console.log(this.$store.state.userInfo.profileUrl);
+          let result = await getUserInfo(this.$store.state.userInfo.profileUrl);
+          this.updateUserInfo(result.data.data);
+          console.log(result);
+        } else if (this.useTo == "forward") {
+          form.append("forwardDynamicId", this.forwardId);
+          console.log("转发了");
+          await SentBlog(form);
+        } else if (this.useTo == "edit") {
+          form.append("id", this.editId);
+          await SentBlog(form);
+        }
+
+        let result = await GetPublic();
+        let publicDynamic = result.data.data;
+        this.updateHomePageDynamic(publicDynamic);
+        // console.log(this.$store.state.HomePageDynamic);
         this.textarea = "";
-        this.urls = [];
+        this.displayUrls = [];
       } else {
         console.log("关键信息不能为空");
       }
@@ -141,14 +187,14 @@ export default {
     },
   },
   created() {
+    console.log(this.textareaHeight);
     //编辑信息不为空 则自动填充编辑信息
     if (JSON.stringify(this.editInfo) !== "{}") {
       this.textarea = this.editInfo.text;
-      // if (this.editInfo.picUrls.length) {
-      //   for (let url of this.editInfo.picUrls) {
-      //     this.urls.push(url);
-      //   }
-      // }
+      if (this.editInfo.Urls) {
+        this.displayUrls = this.editInfo.Urls;
+      } else console.log("图片为空");
+      console.log(this.editInfo.Urls);
       this.editFlag = true;
     } else this.editFlag = false;
   },
@@ -161,6 +207,7 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+
 .back {
   position: fixed;
   left: 0;
@@ -262,6 +309,9 @@ export default {
   color: royalblue;
   font-size: 20px;
 }
+.imgBox {
+  margin: 0 15px;
+}
 .imgBox button {
   position: absolute;
   right: 0;
@@ -280,6 +330,7 @@ export default {
   font-size: 18px;
   color: whitesmoke;
 }
+
 .addBox {
   position: relative;
   border: 2px solid black;
