e63553297ea5639be000f069a8b7a1fd8d231ccd
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-03-28 00:16:24 +0800

    fix: 修复用户信息页点击其他用户名不渲染页面问题

--- frontend/wbapp/src/pages/UserHome.vue
+++ frontend/wbapp/src/pages/UserHome.vue
@@ -1,108 +1,44 @@
 <template>
-  <!-- 无限滚动组件 -->
-  <div
-    class="Home"
-    v-infinite-scroll="load"
-    infinite-scroll-distance="50"
-    style="overflow: auto"
-  >
+  <div>
     <main-layout>
+      <template #userPage> 用户页 </template>
       <template #center>
-        <!-- 动态发布栏 -->
-        <set-bar
-          :showBack="false"
-          :showTop="false"
-          :inputName="inputName"
-          :hideAfterSent="false"
-          ref="homeSetRef"
-          @click="getinfo"
-        >
-        </set-bar>
-        {{ NowTab }}
-        <!-- 测试动态发布 -->
-        <template v-for="n of NowDisNum" :key="n">
-          <dynamic-tab :dynamicInfo="dynamics[n]"></dynamic-tab>
-        </template>
-
-        <!-- <template v-for="n of NowDisNum" :key="n"> -->
-        <!-- :dynamicInfo="dynamics[n]" -->
-        <!-- <dynamic-tab></dynamic-tab> -->
-        <!-- </template> -->
-
-        <!-- 无限加载测试 -->
-
-        <!-- <div v-for="i in count" :key="i" class="test">
-          {{ i }}
-        </div> -->
-        <p v-if="Loading" class="Tips">正在加载中</p>
-        <p v-if="noMore">没有更多数据了</p>
+        <router-view :key="$route.fullPath"></router-view>
       </template>
 
       <template #center-left>
-        <left-bar :tabsArr="tabsArr" @click="updateCom" ref="HomeLeftRef">
-          <template #title>首页</template>
-        </left-bar>
+        <left-bar :ComArr="ComArr"></left-bar>
       </template>
-      <template #center-right><hot-word></hot-word></template>
+      <template #center-right><hot-word></hot-word> </template>
     </main-layout>
   </div>
 </template>
 
 <script>
 import HotWord from "../components/common/HotWord.vue";
-import DynamicTab from "../components/common/Single/DynamicTab.vue";
-// import DynamicTab from "../components/common/Single/DynamicTab.vue";
 import LeftBar from "../components/common/Single/LeftBar.vue";
-import SetBar from "../components/SetBar.vue";
 import MainLayout from "../layouts/MainLayout.vue";
 export default {
-  components: { MainLayout, SetBar, HotWord, LeftBar, DynamicTab },
+  components: { MainLayout, HotWord, LeftBar },
   data() {
     return {
-      count: 0,
-      Loading: false,
-      inputName: "inputFile1",
-      NowTab: "",
-      NowDisNum: 2,
+      show: true,
       dynamics: this.$store.state.userDynamic,
-      tabsArr: [
-        { title: "全部微博", comName: "微博主页" },
-        { title: "最新微博", comName: "最新微博" },
-      ],
+      ComArr: [{ comName: "HomePage1", type: "HomePage" }],
     };
   },
   computed: {
     infos() {
       return this.$store.state.post;
     },
-    noMore() {
-      return this.NowDisNum >= 13;
-    },
   },
-  methods: {
-    getinfo() {
-      let info = this.$refs.homeSetRef.deliverData();
-      alert(info);
-    },
-    updateCom() {
-      this.NowTab = this.$refs.HomeLeftRef.sentComName();
-    },
-
-    // 无限滚动测试
-    load() {
-      if (!this.noMore) {
-        this.Loading = true;
-        setTimeout(() => {
-          this.NowDisNum += 1;
-          this.Loading = false;
-          console.log(this.NowDisNum);
-        }, 1000);
-      } else return;
-    },
-  },
-  created() {
+  async created() {
     console.log("重新请求动态数据");
-    console.log(this.$store.state.userDynamic);
+    // let result = await GetPublic();
+    // let publicDynamic = result.data.data;
+    // console.log(publicDynamic);
+    // this.updateHomePageDynamic(publicDynamic);
+    // console.log(this.$store.state.HomePageDynamic);
   },
 };
 </script>
