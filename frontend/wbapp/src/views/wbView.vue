93cc25bbcc23d390851a1c8d40b378eb25f4acc6
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-04-11 18:32:22 +0800

    feat:搭建新主页（修改路由跳转层级）

---
+++ frontend/wbapp/src/views/wbView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wbView">
+    <main-layout>
+      <template #topNavBar>导航栏区域</template>
+      <template #center-left> <left-bar> </left-bar></template>
+      <template #center>
+        <router-view></router-view>
+      </template>
+      <template #center-right><hot-word></hot-word></template>
+    </main-layout>
+  </div>
+</template>
+
+<script>
+import HotWord from "../components/common/HotWord.vue";
+import LeftBar from "../components/common/Single/LeftBar.vue";
+import MainLayout from "../layouts/MainLayout.vue";
+
+export default {
+  components: { MainLayout, LeftBar, HotWord },
+};
+</script>
+
+<style>
+</style>
